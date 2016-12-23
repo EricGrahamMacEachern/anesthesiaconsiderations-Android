ef5d51f416bff6e8d98dd0dc4edbf01294e8114e
Eric Graham MacEachern <maharg@live.ca>
2016-12-23 03:03:54 -0800

    Did most of E.

--- anesthesiaconsiderations-Android/ExpandingNeckHematoma.cs
+++ anesthesiaconsiderations-Android/ExpandingNeckHematoma.cs
@@ -7,25 +7,111 @@ namespace FormsGallery
     {
         public ExpandingNeckHematoma()
         {
+            Command<Type> navigateCommand =
+                new Command<Type>(async (Type pageType) =>
+                {
+                    Page page = (Page)Activator.CreateInstance(pageType);
+                    await this.Navigation.PushAsync(page);
+                });
+
+            BackgroundColor = Color.White;
+
             Label header = new Label
             {
-                Text = "Exapnding Neck Hematoma",
-                FontSize = 50,
+                Text = "Expanding Neck Hematoma",
+                TextColor = Color.Black,
+                FontSize = 30,
                 FontAttributes = FontAttributes.Bold,
-                HorizontalOptions = LayoutOptions.Center
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
             };
-
             ScrollView scrollView = new ScrollView
             {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                Content = new Label
+                Margin = 0,
+                Padding = 0,
+                Content = new StackLayout
                 {
-                    Text = "Exapnding Neck Hematoma",
+                    Spacing = 0,
+                    Padding = 0,
+                    //Orientation = StackOrientation.Vertical,
+                    Children =
+                    {
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    FontSize = 20,
+                                    Text = "Getting a 404 on this page.",
+                                    TextColor = Color.Black,
+                                    FontAttributes = FontAttributes.Bold,
+                                },
+                                new Label
+                                {
+                                    Text = " ",
+                                    FontSize = 5,
+                                },
+                            }
+                        },
 
-                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "404",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+
+                      new StackLayout
+                        {
+                            Padding = new Thickness(20, 0, 0, 0),
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "​404",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+
+        }
                 }
             };
 
+            Button homeButton = new Button
+            {
+                Text = "Home Page",
+                Command = navigateCommand,
+                CommandParameter = typeof(HomePage),
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
 
 
             // Build the page.
@@ -35,6 +121,7 @@ namespace FormsGallery
                 {
                     header,
                     scrollView,
+                    homeButton,
                 }
             };
         }
