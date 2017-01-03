297cc077f928c4a9f4dc048d364cfc8111896c33
Eric Graham MacEachern <maharg@live.ca>
2017-01-02 16:02:02 -0800

    Did part of O.

--- anesthesiaconsiderations-Android/OmphaloceleAndGastroschisis.cs
+++ anesthesiaconsiderations-Android/OmphaloceleAndGastroschisis.cs
@@ -3,29 +3,114 @@ using Xamarin.Forms;
 
 namespace FormsGallery
 {
-    class OmphaloceleAndGastroschisis : ContentPage
+    class ObstructiveSleepApnea : ContentPage
     {
-        public OmphaloceleAndGastroschisis()
+        public ObstructiveSleepApnea()
         {
+            Command<Type> navigateCommand =
+                new Command<Type>(async (Type pageType) =>
+                {
+                    Page page = (Page)Activator.CreateInstance(pageType);
+                    await this.Navigation.PushAsync(page);
+                });
+
+            BackgroundColor = Color.White;
+
             Label header = new Label
             {
-                Text = "Omphalocele & Gastroschisis",
-                FontSize = 50,
+                Text = "Obstructive Sleep Apnea",
+                TextColor = Color.Black,
+                FontSize = 30,
                 FontAttributes = FontAttributes.Bold,
-                HorizontalOptions = LayoutOptions.Center
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
             };
-
             ScrollView scrollView = new ScrollView
             {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                Content = new Label
+                Margin = 0,
+                Padding = 0,
+                Content = new StackLayout
                 {
-                    Text = "Necrotizing Enterocolitis",
+                    Spacing = 0,
+                    Padding = 0,
+                    //Orientation = StackOrientation.Vertical,
+                    Children =
+                    {
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    FontSize = 20,
+                                    Text = "Considerations",
+                                    TextColor = Color.Black,
+                                    FontAttributes = FontAttributes.Bold,
+                                },
+                                new Label
+                                {
+                                    Text = " ",
+                                    FontSize = 5,
+                                },
+                            }
+                        },
 
-                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "Potentially difficult BMV & intubation",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+
+                        new StackLayout
+                        {
+                            Padding = new Thickness(20, 0, 0, 0),
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "Hypoxia, hypercarbia (chronic)",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+                    }
                 }
             };
 
+            Button homeButton = new Button
+            {
+                Text = "Home Page",
+                Command = navigateCommand,
+                CommandParameter = typeof(HomePage),
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
 
 
             // Build the page.
@@ -35,6 +120,7 @@ namespace FormsGallery
                 {
                     header,
                     scrollView,
+                    homeButton,
                 }
             };
         }
