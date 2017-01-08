59734e0565394a3b2c094ff8963bc84610a4ae6e
Eric Graham MacEachern <maharg@live.ca>
2017-01-07 22:04:45 -0800

    Did more of P.

--- anesthesiaconsiderations-Android/PostpartumHemorrhage.cs
+++ anesthesiaconsiderations-Android/PostpartumHemorrhage.cs
@@ -7,25 +7,173 @@ namespace FormsGallery
     {
         public PostpartumHemorrhage()
         {
+            Command<Type> navigateCommand =
+                new Command<Type>(async (Type pageType) =>
+                {
+                    Page page = (Page)Activator.CreateInstance(pageType);
+                    await this.Navigation.PushAsync(page);
+                });
+
+            BackgroundColor = Color.White;
+
             Label header = new Label
             {
                 Text = "Postpartum Hemorrhage",
-                FontSize = 50,
+                TextColor = Color.Black,
+                FontSize = 30,
                 FontAttributes = FontAttributes.Bold,
-                HorizontalOptions = LayoutOptions.Center
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
             };
-
             ScrollView scrollView = new ScrollView
             {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                Content = new Label
+                Margin = 0,
+                Padding = 0,
+                Content = new StackLayout
                 {
-                    Text = "Postpartum Hemorrhage",
+                    Spacing = 0,
+                    Padding = 0,
+                    //Orientation = StackOrientation.Vertical,
+                    Children =
+                    {
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    FontSize = 20,
+                                    Text = "Considerations",
+                                    TextColor = Color.Black,
+                                    FontAttributes = FontAttributes.Bold,
+                                },
+                                new Label
+                                {
+                                    Text = " ",
+                                    FontSize = 5,
+                                },
+                            }
+                        },
 
-                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "Emergency situation, little time to optimize",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "Physiological changes of pregnancy, Full stomach",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "Determine severity of hemorrhagic shock & resuscitate to goal end-points",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+
+                        new StackLayout
+                        {
+                            Padding = 0,
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "Ddx for PPH:",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+
+                        new StackLayout
+                        {
+                            Padding = new Thickness(20, 0, 0, 0),
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                new Label
+                                {
+                                    Text = "• ",
+                                    TextColor = Color.Black,
+                                },
+                                new Label
+                                {
+                                    FontSize = 16,
+                                    Text = "Ocular massage to dislodge clot",
+                                    TextColor = Color.Black,
+                                    HorizontalOptions = LayoutOptions.Start
+                                },
+                            }
+                        },
+                  }
                 }
             };
 
+            Button homeButton = new Button
+            {
+                Text = "Home Page",
+                Command = navigateCommand,
+                CommandParameter = typeof(HomePage),
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
 
 
             // Build the page.
@@ -35,6 +183,7 @@ namespace FormsGallery
                 {
                     header,
                     scrollView,
+                    homeButton,
                 }
             };
         }
